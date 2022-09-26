683e8fb6c5b44d577c7406976b146e402124b227
dasusekhar4130@gmail.com <dasusekhar4130@gmail.com>
2022-09-26 22:56:57 +0530

    button;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstcomponentComponent } from './firstcomponent/firstcomponent.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"first",component:FirstcomponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
